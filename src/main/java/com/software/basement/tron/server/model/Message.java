e68dd604b6584a84a9a4b876fe3ac645d1989678
akepka <ajmkepka@gmail.com>
2019-05-17 15:31:16 +0200

    Small update to message constructor

--- src/main/java/com/software/basement/tron/server/model/Message.java
+++ src/main/java/com/software/basement/tron/server/model/Message.java
@@ -4,7 +4,8 @@ public class Message {
 
     private String message;
 
-    public Message(String hello) {
+    public Message(String message) {
+        this.message = message;
     }
 
     public String getMessage() {
