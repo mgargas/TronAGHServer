c67b04c5ee8ee93e80e3df29a44bc755a2bae060
Marek Gargas <250497@wp.pl>
2019-05-18 12:27:30 +0200

    Changed project structure

--- src/main/java/com/software/basement/tron/server/Application.java
+++ src/main/java/com/software/basement/tron/server/Application.java
@@ -1,4 +1,4 @@
-package com.software.basement.tron.server.websockets.hello;
+package com.software.basement.tron.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
