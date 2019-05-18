cdb6986b55ff2cd727c6e3efd3eef741e2bdaeba
Rafał Cegielski <cegielski.rafaal@gmail.com>
2019-05-18 21:09:04 +0200

    Add newArrayList

--- src/main/java/com/software/basement/tron/server/websockets/controllers/GameState.java
+++ src/main/java/com/software/basement/tron/server/websockets/controllers/GameState.java
@@ -3,6 +3,7 @@ package com.software.basement.tron.server.websockets.controllers;
 import com.software.basement.tron.server.game.Player;
 import lombok.Data;
 
+import java.util.ArrayList;
 import java.util.Map;
 import java.util.List;
 
@@ -12,6 +13,7 @@ public class GameState {
     private List<PlayerInfo> playersInfo;
 
     public GameState(Map<Integer, Player> playerMap) {
+        playersInfo = new ArrayList<>();
 
         for (Player player : playerMap.values()) {
             new PlayerInfo(player.getId(), player.getPosition());
